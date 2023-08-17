a4df742c1046c524420178380a355134f7d57b73
Sikander-Rasheed99 <sikanderrasheed39@gmail.com>
2023-08-17 11:16:41 +0500

    changes in social app

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import './global.css';
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
 import Login from "./pages/login/Login";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <h1>
       {/* <Home /> */}
-      <BrowserRouter>
+      <HashRouter basename='/socialapp'>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="profile" element={<Profile />} />
           <Route path="login" element={<Login />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </h1>
   );
 }
